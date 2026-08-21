37cac51d461e732d63027c2fd11ed32c59a1979d
pelicarno <peter.pelicarno@gmail.com>
2026-08-21 15:51:47 +0200

    Updated Oauth2 auth and token endpoints to new and improved login page

--- custom_components/olarm/const.py
+++ custom_components/olarm/const.py
@@ -1,7 +1,7 @@
 """Constants for the olarm integration."""
 
 DOMAIN = "olarm"
-OAUTH2_AUTHORIZE = "https://oauth.olarm.com/oauth2/authorize"
-OAUTH2_TOKEN = "https://oauth.olarm.com/oauth2/token"
+OAUTH2_AUTHORIZE = "https://login.olarm.com/#/oauth/authorize"
+OAUTH2_TOKEN = "https://auth.olarm.com/api/v4/oauth2/token"
 OAUTH2_CLIENT_ID = "4at9g5tkr5mpbv9bi2m9qa2dal"
 OAUTH2_CLIENT_SECRET = ""  # Public OAuth client
